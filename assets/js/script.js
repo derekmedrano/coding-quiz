f5bb27162a2297e999832b254f1e340596c6ec1f
Derek Medrano <derekjmedrano@gmail.com>
2023-12-09 14:22:15 -0600

    html structure edited, js linked

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,14 @@ var timerEl = document.querySelectorAll('#timer');
 var quizEl = document.querySelectorAll('#quiz');
 var questionBoxEl = document.querySelectorAll('#question-box');
 var questionEl = document.querySelectorAll('#question');
+var startQuizButtonEl = document.querySelectorAll('#start-quiz');
+
+function startQuiz() {
+    
+console.log("test");
+
+}
 
 
+//Event listeners
+startQuizButtonEl.addEventListener("click", startQuiz);
