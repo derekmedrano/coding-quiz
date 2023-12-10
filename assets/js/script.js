e22aacd629a483f51f1ccf559ecd89d094310e33
Derek Medrano <derekjmedrano@gmail.com>
2023-12-10 16:55:27 -0600

    questions added into object form

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ var score = 0;
 var timerEl = document.getElementById('timer');
 var startQuizButtonEl = document.getElementById('start-quiz');
 var startQuizPromptEl = document.getElementById('quizStartPrompt');
+var quizAnswersEl = document.getElementById('quizQuestions');
+var quizAnswersOpt = document.getElementById('quizAnswersOpt');
 
 function setTime() {
     // Sets interval in variable
@@ -23,16 +25,32 @@ function setTime() {
     }, 1000);
   }
 
+  allQuizQuestions = {
+    'Commonly used data types do NOT include_______' :['1. strings', '2. booleans', '3. alerts', '4. numbers', 2],
+
+    'The condition in an if / else statement is enclosed within_______.' :['1. quotes', '2. curly brackets', '3. square brackets', '4. parentheses', 3],
+
+    'Arrays in JavaScripts can be used to store _________' :['1. numbers and strings', '2. other arrays', '3. booleans', '4. all of the above', 3],
+
+    'String values must be enclosed within ___________ when being assigned to variables' :['1. curly brackets', '2. commas', '3. quotes', '4. parentheses', 0],
+
+    'A very useful tool used during development and debugging for printing content to the debugger is:' :['1. JavaScript', '2. terminal/bash', '3. for loops', '4 console.log', 3]
+
+
+  }
 
 function startQuiz() {   
     console.log("start quiz test log");
     setTime();
+
 
     
 
 
 
 }
+
+
 
 function endQuiz() {
     console.log("end quiz test log");
