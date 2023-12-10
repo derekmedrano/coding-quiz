079024d58587fa11ce10b22d9b9836d47afe636c
Derek Medrano <derekjmedrano@gmail.com>
2023-12-10 15:11:20 -0600

    timer added! quiz ends when time is over

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,45 @@
-// Selector variables
-var viewHighscoresEl = document.querySelectorAll("#highscores");
-var timerEl = document.querySelectorAll('#timer');
-var quizEl = document.querySelectorAll('#quiz');
-var questionBoxEl = document.querySelectorAll('#question-box');
-var questionEl = document.querySelectorAll('#question');
-var startQuizButtonEl = document.querySelectorAll('#start-quiz');
+// Global variables
+var secondsLeft = 100;
+var score = 0;
 
-function startQuiz() {
+// Selector variables
+var timerEl = document.getElementById('timer');
+var startQuizButtonEl = document.getElementById('start-quiz');
+var startQuizPromptEl = document.getElementById('quizStartPrompt');
+
+function setTime() {
+    // Sets interval in variable
+    var timerInterval = setInterval(function() {
+      secondsLeft--;
+      timerEl.textContent = secondsLeft + " seconds remaining";
+  
+      if(secondsLeft === 0) {
+        // Stops execution of action at set interval
+        clearInterval(timerInterval);
+        // Calls function to end quiz and prompt highscore entry
+        endQuiz();
+      }
+  
+    }, 1000);
+  }
+
+
+function startQuiz() {   
+    console.log("start quiz test log");
+    setTime();
+
     
-console.log("test");
+
+
 
 }
+
+function endQuiz() {
+    console.log("end quiz test log");
+
+}
+
+
 
 
 //Event listeners
