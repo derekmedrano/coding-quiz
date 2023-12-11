e6b5cce5d6bc0d2a20531815835c7f7ecbcce0c2
Derek Medrano <derekjmedrano@gmail.com>
2023-12-11 17:07:44 -0600

    basic app finished, no extra styling added

--- assets/js/script2.js
+++ assets/js/script2.js
@@ -7,6 +7,6 @@ return b.secondsLeft - a.secondsLeft;
 for (var i=0; i < scores.length; i++) {
     var score = document.createElement('li');
     score.innerText = scores[i].initials + ' ' + scores[i].secondsLeft;
-    score.classList.add('scoreStyle');
+    score.classList.add('score-style');
     document.getElementById('highscores').appendChild(score);
 }
