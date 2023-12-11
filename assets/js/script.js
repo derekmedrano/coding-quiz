8b5c3dafde11312c40fda1d1b4d4deafa6891653
Derek Medrano <derekjmedrano@gmail.com>
2023-12-10 20:50:54 -0600

    ids changed, obj. changed to array, for loop added

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,8 @@ var quizContainer = document.querySelector("#quizBox");
 var timerEl = document.getElementById('timer');
 
 var quizAnswersEl = document.getElementById('quizQuestions');
-var answerBtn1 = document.getElementById('ansBtn1');
-var answerBtn2 = document.getElementById('ansBtn2');
-var answerBtn3 = document.getElementById('ansBtn3');
-var answerBtn4 = document.getElementById('ansBtn4');
 
-
-
+// Sets timer in header, ends quiz once time reaches 0
 function setTime() {
     // Sets interval in variable
     var timerInterval = setInterval(function() {
@@ -34,28 +29,35 @@ function setTime() {
     }, 1000);
   }
 
-  allQuizQuestions = {
+  // Questions are all stored in an object, properties given to each
+  allQuizQuestions = [
+    {
     question:'Commonly used data types do NOT include_______', 
     options:['1. strings', '2. booleans', '3. alerts', '4. numbers'],
     answer: 2,
-
+    },
+    {
     question:'The condition in an if / else statement is enclosed within_______.',
     options:['1. quotes', '2. curly brackets', '3. square brackets', '4. parentheses'],
     answer: 3,
-
-    question:'Arrays in JavaScripts can be used to store _________',
+    },
+    {
+      question:'Arrays in JavaScripts can be used to store _________',
     options:['1. numbers and strings', '2. other arrays', '3. booleans', '4. all of the above'],
     answer: 3,
-
+    },
+    {
     question:'String values must be enclosed within ___________ when being assigned to variables',
     options:['1. curly brackets', '2. commas', '3. quotes', '4. parentheses'],
     answer: 0,
-
+    },
+    {
     question:'A very useful tool used during development and debugging for printing content to the debugger is:', 
     options: ['1. JavaScript', '2. terminal/bash', '3. for loops', '4 console.log'],
     answer: 3,
+    }
 
-  }
+  ]
 
 
 function startQuiz() {   
@@ -73,9 +75,12 @@ function startQuiz() {
 function insertQuestion() {
   console.log("question test");
 
-  
+  quizAnswersEl.textContent = allQuizQuestions[questionNumber].question;
 
-
+  for (var i = 0; i <=3; i++) {
+    var answerEl = document.getElementById('ansBtn' +i);
+    answerEl.textContent = allQuizQuestions[questionNumber].options[i];
+  }
 
 }
 
